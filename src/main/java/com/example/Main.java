7b4c1da1e5534448b2f8f21ebedb5e84b2e4b3d4
E1psycongr00 <39326175+E1psycongr00@users.noreply.github.com>
2022-06-14 08:47:41 +0900

    B

--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -3,6 +3,6 @@ package com.example;
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println("my power");
+        System.out.println("my pasdfasdfsadf");
     }
 }
