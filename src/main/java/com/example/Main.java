3607850afb11c7858821f7b58a76d7a37ffc0101
E1psycongr00 <39326175+E1psycongr00@users.noreply.github.com>
2022-06-14 09:00:27 +0900

    B 오래걸림

--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -3,6 +3,7 @@ package com.example;
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println("E");
+        System.out.println("B");
+        System.out.println("아 오래걸리네");
     }
 }
