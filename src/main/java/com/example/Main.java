6709a78f3f91e99c492648e244b53453ea251ce4
E1psycongr00 <39326175+E1psycongr00@users.noreply.github.com>
2022-06-14 09:06:02 +0900

    D

--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -3,7 +3,7 @@ package com.example;
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println("E");
+        System.out.println("D");
         System.out.println("이미 완성해버림");
     }
 }
